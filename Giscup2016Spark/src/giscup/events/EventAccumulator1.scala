fbc01afbfac2ae8b099a549f5d27bf6bfa9090ec
Gabor Makrai <makraigabor@gmail.com>
2016-07-27 17:57:42 +0100

    adds EventAccumulator1 class

---
+++ Giscup2016Spark/src/giscup/events/EventAccumulator1.scala
@@ -0,0 +1,48 @@
+package giscup.events
+
+import giscup.tools.SpaceTimeCoordinate
+import scala.collection.mutable.ListBuffer
+
+class EventAccumulator1(bufferSize: Int) {
+  
+  val array: Array[Int] = {
+    Array.fill(bufferSize){ 0 }
+  }
+  
+  def event(stc: SpaceTimeCoordinate) {
+    val index = stc.hashCode()
+    array(index) = array(index) + 1
+  }
+  
+  def event(stc: SpaceTimeCoordinate, i: Int) {
+    val index = stc.hashCode()
+    array(index) = array(index) + i
+  }
+  
+  def getAll(): ListBuffer[Tuple2[SpaceTimeCoordinate, Int]] = {
+    
+    val frequencyList = ListBuffer[Tuple2[SpaceTimeCoordinate, Int]]()
+    
+    for (i <- 0 until array.length) {
+      if (array(i) != 0) {
+        val tuple: Tuple2[SpaceTimeCoordinate, Int] = (SpaceTimeCoordinate.getSpaceTimeCoordinateFromHash(i), array(i))
+        frequencyList += tuple
+      }
+    }
+    
+    frequencyList
+  }
+  
+  def calculateSumX(): Long = {
+    var sum: Long = 0
+    array.foreach(x => sum = sum + x)
+    sum
+  }
+  
+  def calculateSumX2(): Long = {
+    var sum: Long = 0
+    array.foreach(x => sum = sum + x * x)
+    sum
+  }
+  
+}
