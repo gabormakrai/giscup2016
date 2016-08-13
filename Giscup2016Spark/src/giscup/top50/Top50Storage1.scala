7b7e0ed0858ceec0c2d4f8c39451826da03f2b1b
Gabor Makrai <makraigabor@gmail.com>
2016-08-13 23:08:57 +0100

    adds Top50Storage1

---
+++ Giscup2016Spark/src/giscup/top50/Top50Storage1.scala
@@ -0,0 +1,34 @@
+package giscup.top50
+
+import scala.util.Sorting
+import giscup.tools.SpaceTimeCoordinateValue
+
+class Top50Storage1 {
+  
+  val top51: Array[SpaceTimeCoordinateValue] = new Array(51)
+  var top50Index = 0
+  
+  def add(x: Int, y: Int, t: Int, value: Int) {
+    if (top50Index < 50) {
+      top51(top50Index) = new SpaceTimeCoordinateValue(x, y, t, value)
+      top50Index = top50Index + 1
+     if (top50Index == 50) {
+       top51(50) = new SpaceTimeCoordinateValue(x, y, t, value)
+       Sorting.quickSort(top51)
+       top50Index = 51
+     }
+    } else {
+      if (value > top51(50).v) {
+        top51(50) = new SpaceTimeCoordinateValue(x, y, t, value)
+        Sorting.quickSort(top51)
+      }
+    }
+  }
+  
+  def getTop50(): Array[SpaceTimeCoordinateValue] = {
+    val top = new Array[SpaceTimeCoordinateValue](50)
+    Array.copy(top51, 0, top, 0, 50)
+    top
+  }
+  
+}
