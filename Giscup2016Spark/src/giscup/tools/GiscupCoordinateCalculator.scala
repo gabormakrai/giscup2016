4c6633e4ec69a750e608575f09ffe7be7457ebeb
Gabor Makrai <makraigabor@gmail.com>
2016-08-13 15:20:20 +0100

    adds parameters to GiscupCoordinateCalculator

--- Giscup2016Spark/src/giscup/tools/GiscupCoordinateCalculator.scala
+++ Giscup2016Spark/src/giscup/tools/GiscupCoordinateCalculator.scala
@@ -1,4 +1,4 @@
 package giscup.tools
 
-object GiscupCoordinateCalculator extends CoordinateCalculator(-74.25, -73.70, 40.5, 40.9, 0.001, "2015-01-01 00:00:00", "2016-01-01 00:00:00", 7, 25)
+class GiscupCoordinateCalculator(override val degreeParameter: Double, override val timeParameterInDays: Int) extends CoordinateCalculator(-74.25, -73.70, 40.5, 40.9, degreeParameter, "2015-01-01 00:00:00", "2016-01-01 00:00:00", timeParameterInDays, 25)
 
