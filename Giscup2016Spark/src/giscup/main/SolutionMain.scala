1637af73bbd3b554f8925fad97b6d40817417a7f
Gabor Makrai <makraigabor@gmail.com>
2016-08-16 22:53:55 +0100

    updates SolutionMain

--- Giscup2016Spark/src/giscup/main/SolutionMain.scala
+++ Giscup2016Spark/src/giscup/main/SolutionMain.scala
@@ -7,7 +7,7 @@ import giscup.tools.SpaceTimeCoordinate
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import giscup.solutions.Solution
-import giscup.solutions.Solution1
+import giscup.solutions.Solution2
 
 object SolutionMain {
   
@@ -15,26 +15,20 @@ object SolutionMain {
     
     val conf = new SparkConf().setAppName("Giscup G* calculation app")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    conf.registerKryoClasses(Array(classOf[SpaceTimeCoordinate]))
     
     val sc = new SparkContext(conf)
     
-    // TODO: check input dir exists
     val inputFiles = args(0)
     
-    // TODO: check output dir does not exist 
     val outputFile = args(1)
     
-    // TODO: check args(2) is a double    
     val degreeParameter = args(2).toDouble
     
-    // TODO: check that args(3) is an int 
     val timeParameterInDays = args(3).toInt
 
-    val solution: Solution = new Solution1()
+    val solution: Solution = new Solution2()
     
     solution.run(inputFiles, outputFile, degreeParameter, timeParameterInDays, sc)
-    
   }
   
 }
