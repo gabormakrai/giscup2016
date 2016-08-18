5bfe72c1bc5628d8ff108ad5975295205956da0f
Gabor Makrai <makraigabor@gmail.com>
2016-08-18 16:24:03 +0100

    adds comments to LineParser

--- Giscup2016Spark/src/giscup/parser/LineParser.scala
+++ Giscup2016Spark/src/giscup/parser/LineParser.scala
@@ -1,5 +1,11 @@
 package giscup.parser
-
+/**
+ * 
+ * Line Parser trait
+ * 
+ * it only has one function which calculates the event's XYT parameters only if the line contains a valid event 
+ * 
+ */
 trait LineParser {
   def parseLine(xyt: Array[Int], line: String): Boolean
 }
