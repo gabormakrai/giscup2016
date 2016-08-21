22b7eec464511df9f31d68cc608dfecade92b086
Gabor Makrai <makraigabor@gmail.com>
2016-08-21 21:04:07 +0100

    adds comments to SpaceTimeCoordinateValue

--- Giscup2016Spark/src/giscup/tools/SpaceTimeCoordinateValue.scala
+++ Giscup2016Spark/src/giscup/tools/SpaceTimeCoordinateValue.scala
@@ -1,5 +1,12 @@
 package giscup.tools
-
+/**
+ * 
+ * Space-Time coordinate Value class
+ * 
+ * It helps to find the space-time coordinate where most of the events happened
+ * Implements the Ordered trait to support built-in sort functions
+ * 
+ */
 @SerialVersionUID(1L)
 class SpaceTimeCoordinateValue(val x: Int, val y: Int, val t: Int, val v: Int) extends Ordered[SpaceTimeCoordinateValue] with Serializable {
   override def compare(that: SpaceTimeCoordinateValue): Int = {
