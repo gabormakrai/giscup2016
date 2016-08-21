85237db9efdeaa7200dc1579309b089cbd3744b9
Gabor Makrai <makraigabor@gmail.com>
2016-08-21 21:24:49 +0100

    updates CoordinateCalculator to calculate the correct problem size

--- Giscup2016Spark/src/giscup/tools/CoordinateCalculator.scala
+++ Giscup2016Spark/src/giscup/tools/CoordinateCalculator.scala
@@ -11,7 +11,7 @@ class CoordinateCalculator(
     val degreeParameter: Double, 
     val date1: String, 
     val date2: String, 
-    val timeParameterInDays: Int, 
+    val timeParameterInDays: Double, 
     val timePartitions: Int) extends Serializable {
     
   val long0: Double = {
@@ -28,11 +28,15 @@ class CoordinateCalculator(
 		x
   }
   
-  val xMax: Int = (((long3 - long0) / degreeParameter) + 1).toInt
+  val xMax: Int = (((long3 - long0) / degreeParameter) - 1).toInt
   
   val xSize: Int = {
     SpaceTimeCoordinate.xSize = xMax + 1
     xMax + 1
+  }
+  
+  val longitudeOffset: Int = {
+    (long0 / degreeParameter).toInt
   }
   
   val lat0: Double = {
@@ -49,25 +53,51 @@ class CoordinateCalculator(
 		x
   }
 		
-	val yMax: Int = (((lat3 - lat0) / degreeParameter) + 1).toInt
+	val yMax: Int = (((lat3 - lat0) / degreeParameter) - 1).toInt
 	
 	val ySize: Int = {
 	  SpaceTimeCoordinate.ySize = yMax + 1
 	  yMax + 1
 	}
+	
+  val latitudeOffset: Int = {
+    (lat0 / degreeParameter).toInt
+  }	
 				
-	val timeParameter = 24 * 3600 * 1000 * timeParameterInDays
+	val timeParameter = (24.0 * 3600.0 * 1000.0 * timeParameterInDays).toLong
 	
 	val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 	
-	val time1: Long = sdf.parse(date1).getTime()
+	val timeOrigin = 1420070400000L //sdf.parse("2015-01-01 00:00:00").getTime
 	
-	val tMax: Int = ((sdf.parse(date2).getTime() - time1) / (timeParameter)).toInt
-
+	val time1 = sdf.parse(date1).getTime
+	
+	val time2 = sdf.parse(date2).getTime
+	
+  val time0: Long = {
+		var x = ((time1 - timeOrigin) / timeParameter).toLong
+		x = x * timeParameter
+ 		if (x > time1) x -= timeParameter
+		x
+  }
+  		
+  val time3: Long = {
+		var x = ((time2 - timeOrigin) / timeParameter).toLong
+		x = x * timeParameter
+		if (x < time2) x += timeParameter
+		x
+  }
+  
+  val tMax: Int = (((time3 - time0) / timeParameter) - 1).toInt  
+  
 	val tSize: Int = {
 	  SpaceTimeCoordinate.tSize = tMax + 1
 	  tMax + 1
 	}
+  
+  val timeOffset: Int = {
+    (time0 / timeParameter).toInt
+  }  
 	
 	def getX(longitude: Double): Int = {
 	  if (longitude < long0 || longitude > long3) -1
@@ -79,23 +109,47 @@ class CoordinateCalculator(
 	  else Math.floor((latitude - lat0) / degreeParameter).toInt
 	}
 	
-	def getT(time: Long): Int = ((time - time1) / this.timeParameter).toInt
-
-	def getTimePartition(t: Int, partitionIds: Array[Int]) = {
-		val p0 = (t.toDouble / tSize.toDouble * (timePartitions)).toInt;
-		val p1 = ((t.toDouble - 1.0) / tSize.toDouble * (timePartitions)).toInt
-		val p2 = ((t.toDouble + 1.0) / tSize.toDouble * (timePartitions)).toInt
-		
-		if (p0 != p1 && p0 == p2) {
-			partitionIds(0) = p0
-			partitionIds(1) = p0- 1
-		} else if (t != tMax && p0 != p2 && p0 == p1) {
-			partitionIds(0) = p0
-			partitionIds(1) = p0 + 1
-		} else {
-			partitionIds(0) = p0
-			partitionIds(1) = -1
-		}
+	def getT(time: Long): Int = {
+	  val time5 = time - timeOrigin
+	  if (time5 < time0 || time5> time3) -1
+	  else Math.floor((time5 - time0) / timeParameter).toInt
+	}
+	
+//	def getTimePartition(t: Int, partitionIds: Array[Int]) = {
+//		val p0 = (t.toDouble / tSize.toDouble * (timePartitions)).toInt;
+//		val p1 = ((t.toDouble - 1.0) / tSize.toDouble * (timePartitions)).toInt
+//		val p2 = ((t.toDouble + 1.0) / tSize.toDouble * (timePartitions)).toInt
+//		
+//		if (p0 != p1 && p0 == p2) {
+//			partitionIds(0) = p0
+//			partitionIds(1) = p0- 1
+//		} else if (t != tMax && p0 != p2 && p0 == p1) {
+//			partitionIds(0) = p0
+//			partitionIds(1) = p0 + 1
+//		} else {
+//			partitionIds(0) = p0
+//			partitionIds(1) = -1
+//		}
+//	}
+	
+	def calculateLongitudeRange(x: Int): Array[Double] = {
+	  val returnArray = new Array[Double](2)
+	  returnArray(0) = long0 + x * degreeParameter
+	  returnArray(1) = long0 + (x+1) * degreeParameter
+	  returnArray
 	}
   
+	def calculateLatitudeRange(y: Int): Array[Double] = {
+	  val returnArray = new Array[Double](2)
+	  returnArray(0) = lat0 + y * degreeParameter
+	  returnArray(1) = lat0 + (y+1) * degreeParameter
+	  returnArray
+	}
+	
+	def calculateTimeRange(t: Int): Array[Long] = {
+	  val returnArray = new Array[Long](2)
+	  returnArray(0) = timeOrigin + time0 + t * timeParameter
+	  returnArray(1) = timeOrigin + time0 + (t+1) * timeParameter
+	  returnArray
+	}
 }
